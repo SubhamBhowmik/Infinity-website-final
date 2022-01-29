63913c6eb24d90560a91e41350a7700674cffa0f
Subham <czsubham@gmail.com>
2022-01-29 12:33:44 +0600

    responsive donate us

--- infinity/src/Component/DonateUs.js
+++ infinity/src/Component/DonateUs.js
@@ -2,51 +2,51 @@ import React from 'react';
 import { useEffect } from 'react';
 import Fade from "react-reveal"
 import "./donate.css"
-import Footer from  "./Footer"
+import Footer from "./Footer"
 const DonateUs = () => {
-    useEffect(() => {
-        window.scrollTo(0,0);  
-        
-        }, [])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+  }, [])
   return (
     <>
-    <div class="container py-5">
+      <div class="container py-5">
         <div class='middletext'>
-        <Fade right>
-        <h2 class="mb-4">We work to make quality education accessible to all.</h2>
-        <h4 class="h6 mb-1 text-uppercase text-primary">Donate to support our cause.</h4>
-        </Fade>
+          <Fade right>
+            <h2 class="mb-4">We work to make quality education accessible to all.</h2>
+            <h4 class="h6 mb-1 text-uppercase text-primary">Donate to support our cause.</h4>
+          </Fade>
         </div>
       </div>
-      
-     <div className="main">
-       <div className="container-">
-           <div className="main-card">
-           <Fade left duration={1200}>
-           <img src="./assets/img/QRCode.jpg" alt="" />
-           </Fade>
-            
-             <div className="typo">
-             <Fade right >
-             <h5>Name:-</h5>
-             <h6>INFINITY FOUNDATION</h6>
-         
-             <h5>Account No:-</h5>
-             <h6>60402614940</h6>
-            
-           
-              <h5>IFSC CODE:-</h5>
-              <h6>MAHB0000041</h6>
-              <h5>Bank name:-</h5>
-              <h6>Bank of Maharashtra</h6>
+
+      <div className="main">
+        <div className="container-">
+          <div className="main-card ">
+            <Fade left duration={1200}>
+              <img src="./assets/img/QRCode.jpg" alt="" />
+            </Fade>
+
+            <div className='typo' >
+              <Fade right >
+                <h5>Name:-</h5>
+                <h6>INFINITY FOUNDATION</h6>
+
+                <h5>Account No:-</h5>
+                <h6>60402614940</h6>
+
+
+                <h5>IFSC CODE:-</h5>
+                <h6>MAHB0000041</h6>
+                <h5>Bank name:-</h5>
+                <h6>Bank of Maharashtra</h6>
               </Fade>
-             </div>
-           
-           </div>
-       </div>
-    
-     </div>
-     <Footer/>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+      <Footer />
     </>
   )
 };
--- infinity/src/Component/TalentForum.js
+++ infinity/src/Component/TalentForum.js
@@ -1,11 +1,11 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import Fade from 'react-reveal'
 import './talentforum.css'
-import  Footer from './Footer'
+import Footer from './Footer'
 const Forum = () => {
     useEffect(() => {
         window.scrollTo(0, 0)
-      }, [])
+    }, [])
     return (
         <>
             <div class="listing-hero">
@@ -25,21 +25,21 @@ const Forum = () => {
             </div>
 
             <div class="container py-5">
-            <div class='middletext'>
-            <Fade right>
-            <h2 class="mb-4">Why subscribe to Infinity Talent Forum</h2>
-            <p class="text-body">Through Inifnity Talent Forum, we are working to create strong foundation among students so they can compete in the future. We provide books with the latest materials, doubt clearing sessions and regular exams to monitor progress. With the help of ITF books and Exams, every student gets a platform to deepen their understanding of concepts and perform their knowledge. Check out our prospectus and fill up teh Registration Form, and we will get in touch with you.</p>
-            </Fade>
+                <div class='middletext'>
+                    <Fade right>
+                        <h2 class="mb-4">Why subscribe to Infinity Talent Forum</h2>
+                        <p class="text-body">Through Inifnity Talent Forum, we are working to create strong foundation among students so they can compete in the future. We provide books with the latest materials, doubt clearing sessions and regular exams to monitor progress. With the help of ITF books and Exams, every student gets a platform to deepen their understanding of concepts and perform their knowledge. Check out our prospectus and fill up teh Registration Form, and we will get in touch with you.</p>
+                    </Fade>
+                </div>
+
+                <img class="pros-image" src="./assets/img/page1.png" />
+                <img class="pros-image" src="./assets/img/page2.png" />
             </div>
 
-            <img class="pros-image" src="./assets/img/page1.png" />
-            <img class="pros-image" src="./assets/img/page2.png" />
-            </div>
-            
             <div class='middletext'>
-            <h2 class="mb-4">REGISTRATION FORM</h2>
+                <h2 class="mb-4">REGISTRATION FORM</h2>
             </div>
-            
+
 
             <div class="container-contact">
                 <div class="wrap-contact">
@@ -73,22 +73,22 @@ const Forum = () => {
                         <Fade left>
                             <div class="form-check form-check-inline ">
                                 Gender:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <input class="form-check-input" type="radio" name="Gender" id="male" value="male"/>
-                                    <label class="form-check-label" for="male">Male</label>
-                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <input class="form-check-input" type="radio"  name="Gender" id="female" value="female"/>
-                                    <label class="form-check-label" for="female">Female</label>
-                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <input class="form-check-input" type="radio"  name="Gender" id="female" value="female"/>
-                                    <label class="form-check-label" for="female">Other</label>
-                                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <input class="form-check-input" type="radio"  name="Gender" id="female" value="female"/>
-                                    <label class="form-check-label" for="female">Do not want to mention</label>
+                                <input class="form-check-input" type="radio" name="Gender" id="male" value="male" />
+                                <label class="form-check-label" for="male">Male</label>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <input class="form-check-input" type="radio" name="Gender" id="female" value="female" />
+                                <label class="form-check-label" for="female">Female</label>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <input class="form-check-input" type="radio" name="Gender" id="female" value="female" />
+                                <label class="form-check-label" for="female">Other</label>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                <input class="form-check-input" type="radio" name="Gender" id="female" value="female" />
+                                <label class="form-check-label" for="female">Do not want to mention</label>
                             </div>
 
                         </Fade>
 
-                        <Fade  left>
+                        <Fade left>
                             <div class="wrap-input validate-input mt-5 " data-validate="Please enter your Address">
                                 <input class="input mt-3" type="text" required="true" required="true" name="address" placeholder="Address" />
                             </div>
@@ -105,7 +105,7 @@ const Forum = () => {
 
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </>
     )
 }
